fe873cd51539d356db790fa1d792c451d5a07523
Vamsi Krishna Sridhara <vamsisridhara@gmail.com>
2019-12-10 15:51:00 -0600

    Update app.module.ts
    commit test from git

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule } from '@angular/core'; //test commit
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
