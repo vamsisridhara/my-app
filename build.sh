184e7ab6a8f2b55de10d120da752ddca25c781d8
Vamsi Krishna Sridhara <vamsisridhara@gmail.com>
2020-02-07 21:00:13 -0600

    final push

--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
-
 ng build --prod --configuration=$1 --build-optimizer false --output-hashing all && gzipper -v ./dist
-
+ng build --prod --configuration=production --build-optimizer false --output-hashing all && gzipper -v ./dist
